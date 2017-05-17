240800c41f347743fee58d6e52a6b177fb68b812
Tamara Rivera <tamita.rivera@gmail.com>
2017-05-16 21:44:29 -0700

    Added the population of the table with the form.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 var items = [];
 
 function toggleSections() {
-	$('#itemsTable table').toggleClass('hidden');
-	$('#addItem').toggleClass('hidden');
+	$('#itemsTable').toggleClass('hidden');
 	$('#addForm').toggleClass('hidden');
 }
 
@@ -20,11 +19,30 @@ function clearInputs() {
 $('#formSubmit').on('click', formSubmited);
 
 function formSubmited() {
-	if($('#description').val() && $('#quantity').val()) {
+	var description = $('#description').val();
+	var quantity = $('#quantity').val();
+	if(description && quantity) {
+		items.push({
+			'description': description,
+			'quantity': quantity
+		});
+		populateTable();
 		toggleSections();
 		clearInputs();
 	}
 	event.preventDefault();
+}
+
+function populateTable() {
+	var table = $('#itemsTable table tbody');
+	table.html('');
+	items.forEach(function (item, index) {
+		var row = $('<tr><td>'+ item.description +'</td><td>' + item.quantity + '</td></tr>');
+		row.append('<button>Edit</button>');
+		row.append('<button>Delete</button>');
+		table.append(row);
+	});
+
 }
 
 $('#formCancel').on('click', function () {

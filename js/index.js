57772b8f86e55be5dc702e047f0b1ef5118b6b2e
Tamara Rivera <tamita.rivera@gmail.com>
2017-05-16 18:02:42 -0700

    Added the toggle functionality for the two sections of the page.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 var items = [];
+
+function toggleSections() {
+	$('#itemsTable table').toggleClass('hidden');
+	$('#addItem').toggleClass('hidden');
+	$('#addForm').toggleClass('hidden');
+}
+
+$('#addItem').on('click', addItem);
+
+function addItem() {
+	toggleSections();
+}
+
+function clearInputs() {
+	$('#description').val('');
+	$('#quantity').val('');
+}
+
+$('#formSubmit').on('click', formSubmited);
+
+function formSubmited() {
+	if($('#description').val() && $('#quantity').val()) {
+		toggleSections();
+		clearInputs();
+	}
+	event.preventDefault();
+}
+
+$('#formCancel').on('click', function () {
+	clearInputs();
+	event.preventDefault();
+});
